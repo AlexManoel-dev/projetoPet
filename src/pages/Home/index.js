fcf7d51c1a4290377fcf19ae982788a008d6b6af
Alex Manoel <alexmanoel93@gmail.com>
2021-07-21 17:09:15 -0300

    Tela home em andamento

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import styles from './styles'
 
 export default function(){
     return(
-        <Text>Home</Text>
+        <View style={styles.container}>
+            <View>
+                {/* Colocar imagem */}
+                <Text>Seja bem-vindo,</Text>
+                <Text style={styles.txt}>Confira as novidades!</Text>
+
+                <View>
+                    <Text>Notícias</Text>
+                    <View style={styles.noticias}></View>
+                </View>
+
+                <View>
+                    <View>
+                        <Text>Perdidos/Roubados</Text>
+                        <View style={styles.viewsFinais}>
+
+                        </View>
+                    </View>
+                    <View>
+
+                    </View>
+                        <Text>Localizados</Text>
+                        <View style={styles.viewsFinais}>
+
+                        </View>
+                    </View>
+            </View>
+        </View>
     )
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,5 +1,28 @@
 import { StyleSheet } from 'react-native'
 
 export default StyleSheet.create({
-    
+    container:{
+        paddingTop: 25
+    },
+    txt:{
+        fontWeight: 'bold',
+        marginBottom: 10
+    },
+    noticias:{
+        borderWidth: 1,
+        borderColor: 'black',
+        height: 150,
+        marginRight: 20,
+        marginLeft: 20,
+        borderRadius: 20,
+        marginTop: 3,
+    },
+    viewsFinais:{
+        borderWidth: 1,
+        borderColor: 'black',
+        height: 150,
+        width: 150,
+        borderRadius: 20,
+        marginTop: 3,
+    }
 })
